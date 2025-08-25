7004df48fd332536d0fed98ab1f49211576402cd
Jayaraj U B <ubelamagi@gmail.com>
2025-08-25 11:02:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors({ origin: "https://movietracker-brown.vercel.app/", credentials: true }));
 app.use(bodyParser.json());
 app.use(
   session({
